dfa2f02b183f8331b00d929154dc78db8b46677f
Andy Trinh <andy.thienantrinh@gmail.com>
2023-09-21 17:54:15 -0400

    Initial Commit

---
+++ Highlighter/HighlighterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighlighterApp.swift
+//  Highlighter
+//
+//  Created by Andy Trinh on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct HighlighterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

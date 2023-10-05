aceab3f7fd85c3dc79bb00ad75c7730d751da503
Andy Trinh <andy.thienantrinh@gmail.com>
2023-10-04 22:55:21 -0400

    Temporarily removed persistence data

--- Highlighter/HighlighterApp.swift
+++ Highlighter/HighlighterApp.swift
@@ -9,26 +9,27 @@ import SwiftUI
 
 @main
 struct HighlighterApp: App {
-    @StateObject private var store = HighlightStore()
+//    @StateObject private var store = HighlightStore()
+    @State private var highlights = Highlight.sampleData
     
     var body: some Scene {
         WindowGroup {
-            HighlightsView(highlights: $store.highlights) {
-                Task {
-                    do {
-                        try await store.save(highlights: store.highlights)
-                    } catch {
-                        fatalError(error.localizedDescription)
-                    }
-                }
+            HighlightsView(highlights: $highlights) {
+//                Task {
+//                    do {
+//                        try await store.save(highlights: store.highlights)
+//                    } catch {
+//                        fatalError(error.localizedDescription)
+//                    }
+//                }
             }
-                .task {
-                    do {
-                        try await store.load()
-                    } catch {
-                        fatalError(error.localizedDescription)
-                    }
-                }
+//                .task {
+//                    do {
+//                        try await store.load()
+//                    } catch {
+//                        fatalError(error.localizedDescription)
+//                    }
+//                }
         }
     }
 }

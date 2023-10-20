b5398f04ab03dee42c6f644b32b556f18ae5a1aa
Andy Trinh <andy.thienantrinh@gmail.com>
2023-10-20 15:49:37 -0400

    Update sharedviewcontroller

--- HighlighterShareExtension/ShareViewController.swift
+++ HighlighterShareExtension/ShareViewController.swift
@@ -8,28 +8,37 @@
 
 import UIKit
 import Social
+import Firebase
 
 
 class ShareViewController: SLComposeServiceViewController {
-    private var textString: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
+        // Get the item provider for text
+        if let item = extensionContext?.inputItems.first as? NSExtensionItem,
+           let itemProvider = item.attachments?.first as? NSItemProvider,
+           itemProvider.hasItemConformingToTypeIdentifier("public.plain-text") {
+            
+            itemProvider.loadItem(forTypeIdentifier: "public.plain-text", options: nil) { [weak self] (result, error) in
+                if let text = result as? String {
+                    self?.uploadTextToFirebase(text)
+                }
+            }
+        }
     }
 
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-        print("User just posted")
-
-    
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    func uploadTextToFirebase(_ text: String) {
+        // Your Firebase Realtime Database code here.
+        // For example:
+        let dbRef = Database.database().reference().child("highlights")
+        dbRef.childByAutoId().setValue(["content": text]) { (error, _) in
+            if let error = error {
+                print("Error uploading to Firebase: \(error)")
+            }
+            self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+        }
     }
 
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
-    }
 
 }

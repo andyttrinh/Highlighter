a0b3cba99d013b26fcced3670b7938fae5a8399a
Andy Trinh <andy.thienantrinh@gmail.com>
2023-10-23 18:02:56 -0400

    Share Sheet shows a Highlight Saved View when saved

---
+++ HighlighterShareExtension/ShareExtensionView.swift
@@ -0,0 +1,18 @@
+//
+//  ShareExtensionView.swift
+//  HighlighterShareExtension
+//
+//  Created by Andy Trinh on 10/23/23.
+//
+
+import SwiftUI
+
+struct ShareExtensionView: View {
+    var body: some View {
+        Text("Highlight Saved")
+    }
+}
+
+#Preview {
+    ShareExtensionView()
+}
--- HighlighterShareExtension/ShareViewController.swift
+++ HighlighterShareExtension/ShareViewController.swift
@@ -12,49 +12,40 @@ import MobileCoreServices
 import UniformTypeIdentifiers
 import Firebase
 import HighlighterShared
+import SwiftUI
 
 
-class ShareViewController: SLComposeServiceViewController {
+
+
+class ShareViewController: UIViewController {
     
     override func viewDidLoad() {
-            super.viewDidLoad()
-        
-            // Initialize Firebase
-            FirebaseApp.configure()
+        super.viewDidLoad()
 
-            if let item = extensionContext?.inputItems.first as? NSExtensionItem {
-                if let textItemProvider = item.attachments?.first as? NSItemProvider {
-                    if textItemProvider.hasItemConformingToTypeIdentifier(UTType.plainText.identifier) {
-                        textItemProvider.loadItem(forTypeIdentifier: UTType.plainText.identifier, options: nil, completionHandler: { [weak self] (text, error) in
-                            DispatchQueue.main.async {
-                                if let sharedText = text as? String {
-                                    // Handle text
-                                    let newHighlight = Highlight(source: "External App", content: sharedText, labels: [])
-                                    self!.uploadHighlight(highlight: newHighlight)
-                                    print(sharedText)
-                                }
-                                self?.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+        // Initialize Firebase
+        FirebaseApp.configure()
+
+        if let item = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let textItemProvider = item.attachments?.first as? NSItemProvider {
+                if textItemProvider.hasItemConformingToTypeIdentifier(UTType.plainText.identifier) {
+                    textItemProvider.loadItem(forTypeIdentifier: UTType.plainText.identifier, options: nil, completionHandler: { [weak self] (text, error) in
+                        DispatchQueue.main.async {
+                            if let sharedText = text as? String {
+                                let newHighlight = Highlight(source: "External App", content: sharedText, labels: [])
+                                self!.uploadHighlight(highlight: newHighlight)
+                                print(sharedText)
+                                // Handle your text and do other necessary actions here
+                                // Only when you want to dismiss your extension, call the completion handler
+                                 self?.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
                             }
-                        })
-                    }
+                        }
+                    })
                 }
             }
         }
-    
-    override func didSelectPost() {
-        print("Posted")
-//        if let contentText = self.contentText {
-//                print(contentText)
-//                let userDefaults = UserDefaults(suiteName: "group.com.yourcompany.TextShareApp")
-//                        userDefaults?.set(contentText, forKey: "sharedText")
-//                        userDefaults?.synchronize()  // Force saving
-//                // Handle the shared content as you wish, e.g., save it, send to server, etc.
-//            }
-//
-//        // Inform the host app that we're done
-//        extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
     }
 
+    
     func uploadHighlight(highlight: Highlight) {
         let dbRef = Database.database().reference()
         let highlightRef = dbRef.child("highlights").child(highlight.id.uuidString)
@@ -68,6 +59,35 @@ class ShareViewController: SLComposeServiceViewController {
             print("Error encoding highlight: \(error)")
         }
     }
+    
+    func hostingControllerForShareExtensionView() -> UIViewController {
+        let view = ShareExtensionView()
+        let hostingController = UIHostingController(rootView: view)
+        return hostingController
+    }
+    
+    override func loadView() {
+        super.loadView()  // Call the super implementation
+        
+        let uiHostingController = hostingControllerForShareExtensionView()
+        
+        // Add the hosting controller as a child view controller
+        self.addChild(uiHostingController)
+        self.view.addSubview(uiHostingController.view)
+        
+        // Set up constraints to make the hosting controller's view fill the entire view
+        uiHostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        uiHostingController.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        uiHostingController.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        uiHostingController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        uiHostingController.view.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        
+        // Call the necessary containment methods
+        uiHostingController.didMove(toParent: self)
+    }
+
+
+
 
 
 }
